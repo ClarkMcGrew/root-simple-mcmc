517a53e2eea2596d5a37a907d302cda1531e6425
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-07-20 12:18:55 -0400

    Draw the picture to png as well as saving the ROOT file

--- MakeCovariance.C
+++ MakeCovariance.C
@@ -17,7 +17,7 @@
 // histograms:
 //
 // AcceptedCovariance (TH2D) -- A 2 D histogram with the bin values
-//     representing the covariance (bin 1 is for Accepted[0]).  
+//     representing the covariance (bin 1 is for Accepted[0]).
 //
 // AcceptedMean (TProfile) -- A 1 D profile histogram with the bin values
 //     representing the means (bin 1 is Accepted[0]).  The errors on the
@@ -35,7 +35,7 @@ void MakeCovariance() {
         if (std::string(key->GetClassName()) != "TTree") continue;
         name = key->GetName();
     }
-    
+
     // Get the tree out of the file.
     TTree *inputTree = (TTree*) gFile->Get(name.c_str());
     std::cout << "Input Tree Name: " << inputTree->GetName() << std::endl;
@@ -58,11 +58,12 @@ void MakeCovariance() {
     TProfile* mean = new TProfile("AcceptedMean",
                                   "Mean value of the Accepted Points",
                                   dim, 0, dim,"S");
-    
+
     // Calculate the average and covariance.
     std::vector<double> avg(dim);
     for(int e=0; e<entries; ++e) {
         inputTree->GetEntry(e);
+        if (e%1000 == 0) std::cout << "entry " << e << std::endl;
         for (int i = 0; i<accepted->size(); ++i) {
             mean->Fill(i+0.1,accepted->at(i));
             avg[i] += accepted->at(i);
@@ -72,18 +73,28 @@ void MakeCovariance() {
             }
         }
     }
+
     for (int i=0; i<dim; ++i) avg[i] /= entries;
+    double minCov = 0.0;
+    double maxCov = 0.0;
     for (int i=0; i<dim; ++i) {
         for (int j=0; j<dim; ++j) {
             double v = covariance->GetBinContent(i+1,j+1);
             // This assumes entries is very large...
             v = v/entries - avg[i]*avg[j];
             covariance->SetBinContent(i+1,j+1,v);
+            minCov = std::min(minCov,v);
+            maxCov = std::max(maxCov,v);
         }
     }
 
     // Save it to a file.
+    covariance->SetMinimum(minCov);
+    covariance->SetMaximum(maxCov);
+    covariance->SetStats(false);
+    covariance->Draw("colz");
+    gPad->Print("AcceptedCovariance.png");
+
     covariance->Write();
     mean->Write();
-    output.Close();
 }

b91d06786aad7e638682e6ebb69d016dca9f0960
Clark McGrew <clark.mcgrew@stonybrook.edu>
2022-05-09 16:29:26 -0400

    Compile with optimization

--- mcmc-compile.sh
+++ mcmc-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$(root-config --cxx) $(root-config --cflags) \
+$(root-config --cxx) $(root-config --cflags) -O2 \
 		     -o mcmc.exe \
 		     -DMAIN_PROGRAM SimpleMCMC.C \
 		     $(root-config --libs)
